59e7d42eaa7303ea6f8ed4aacb4026282a1ded7d
felid <felid.codas@gmail.com>
2019-07-05 13:04:42 +0900

    面の描画をコーディング中。時間の関係でまだ直方体の上面の描画しか書けてない。

--- work_BF3D01/Cube.cpp
+++ work_BF3D01/Cube.cpp
@@ -25,93 +25,114 @@ void Cube::Move(VECTOR velocity)
 
 void Cube::Draw() const
 {
-	VECTOR copyVertex = mVertex;
-	VECTOR copyInversion = mInversionVertex;
-
-	// X方向線分描画
+	// 線で描画
 	{
-		// 一本目
-		copyInversion.y = mVertex.y;
-		copyInversion.z = mVertex.z;
-		DrawLine3D(copyVertex, copyInversion, mColor);
-		copyInversion = mInversionVertex;
+		VECTOR copyVertex = mVertex;
+		VECTOR copyInversion = mInversionVertex;
 
-		// 二本目
-		copyVertex.y = mInversionVertex.y;
-		copyInversion.z = mVertex.z;
-		DrawLine3D(copyVertex, copyInversion, mColor);
-		copyVertex = mVertex;
-		copyInversion = mInversionVertex;
+		// X方向線分描画
+		{
+			// 一本目
+			copyInversion.y = mVertex.y;
+			copyInversion.z = mVertex.z;
+			DrawLine3D(copyVertex, copyInversion, mColor);
+			copyInversion = mInversionVertex;
 
-		// 三本目
-		copyVertex.y = mInversionVertex.y;
-		copyVertex.z = mInversionVertex.z;
-		DrawLine3D(copyVertex, copyInversion, mColor);
-		copyVertex = mVertex;
+			// 二本目
+			copyVertex.y = mInversionVertex.y;
+			copyInversion.z = mVertex.z;
+			DrawLine3D(copyVertex, copyInversion, mColor);
+			copyVertex = mVertex;
+			copyInversion = mInversionVertex;
 
-		// 四本目
-		copyInversion.y = mVertex.y;
-		copyVertex.z = mInversionVertex.z;
-		DrawLine3D(copyVertex, copyInversion, mColor);
-		copyVertex = mVertex;
-		copyInversion = mInversionVertex;
+			// 三本目
+			copyVertex.y = mInversionVertex.y;
+			copyVertex.z = mInversionVertex.z;
+			DrawLine3D(copyVertex, copyInversion, mColor);
+			copyVertex = mVertex;
+
+			// 四本目
+			copyInversion.y = mVertex.y;
+			copyVertex.z = mInversionVertex.z;
+			DrawLine3D(copyVertex, copyInversion, mColor);
+			copyVertex = mVertex;
+			copyInversion = mInversionVertex;
+		}
+
+		// Y方向線分描画
+		{
+			// 一本目
+			copyInversion.x = mVertex.x;
+			copyInversion.z = mVertex.z;
+			DrawLine3D(copyVertex, copyInversion, mColor);
+			copyInversion = mInversionVertex;
+
+			// 二本目
+			copyVertex.x = mInversionVertex.x;
+			copyInversion.z = mVertex.z;
+			DrawLine3D(copyVertex, copyInversion, mColor);
+			copyVertex = mVertex;
+			copyInversion = mInversionVertex;
+
+			// 三本目
+			copyVertex.x = mInversionVertex.x;
+			copyVertex.z = mInversionVertex.z;
+			DrawLine3D(copyVertex, copyInversion, mColor);
+			copyVertex = mVertex;
+
+			// 四本目
+			copyInversion.x = mVertex.x;
+			copyVertex.z = mInversionVertex.z;
+			DrawLine3D(copyVertex, copyInversion, mColor);
+			copyVertex = mVertex;
+			copyInversion = mInversionVertex;
+		}
+
+		// Z方向線分描画
+		{
+			// 一本目
+			copyInversion.x = mVertex.x;
+			copyInversion.y = mVertex.y;
+			DrawLine3D(copyVertex, copyInversion, mColor);
+			copyInversion = mInversionVertex;
+
+			// 二本目
+			copyVertex.x = mInversionVertex.x;
+			copyInversion.y = mVertex.y;
+			DrawLine3D(copyVertex, copyInversion, mColor);
+			copyVertex = mVertex;
+			copyInversion = mInversionVertex;
+
+			// 三本目
+			copyVertex.x = mInversionVertex.x;
+			copyVertex.y = mInversionVertex.y;
+			DrawLine3D(copyVertex, copyInversion, mColor);
+			copyVertex = mVertex;
+
+			// 四本目
+			copyInversion.x = mVertex.x;
+			copyVertex.y = mInversionVertex.y;
+			DrawLine3D(copyVertex, copyInversion, mColor);
+			copyVertex = mVertex;
+			copyInversion = mInversionVertex;
+		}
 	}
 
-	// Y方向線分描画
+	// 面を描画
+	// 三角形を二つ組み合わせて描画する
 	{
-		// 一本目
-		copyInversion.x = mVertex.x;
-		copyInversion.z = mVertex.z;
-		DrawLine3D(copyVertex, copyInversion, mColor);
-		copyInversion = mInversionVertex;
+		// 描画する三角形の頂点
+		VECTOR vertex[3];
 
-		// 二本目
-		copyVertex.x = mInversionVertex.x;
-		copyInversion.z = mVertex.z;
-		DrawLine3D(copyVertex, copyInversion, mColor);
-		copyVertex = mVertex;
-		copyInversion = mInversionVertex;
+		// 上面
+		{
+			vertex[0] = VGet(mVertex.x, mVertex.y, mInversionVertex.z);
+			vertex[1] = mVertex;
+			vertex[2] = VGet(mInversionVertex.x, mVertex.y, mInversionVertex.z);
+			DrawTriangle3D(vertex[0], vertex[1], vertex[2], mColor, TRUE);
 
-		// 三本目
-		copyVertex.x = mInversionVertex.x;
-		copyVertex.z = mInversionVertex.z;
-		DrawLine3D(copyVertex, copyInversion, mColor);
-		copyVertex = mVertex;
-
-		// 四本目
-		copyInversion.x = mVertex.x;
-		copyVertex.z = mInversionVertex.z;
-		DrawLine3D(copyVertex, copyInversion, mColor);
-		copyVertex = mVertex;
-		copyInversion = mInversionVertex;
-	}
-
-	// Z方向線分描画
-	{
-		// 一本目
-		copyInversion.x = mVertex.x;
-		copyInversion.y = mVertex.y;
-		DrawLine3D(copyVertex, copyInversion, mColor);
-		copyInversion = mInversionVertex;
-
-		// 二本目
-		copyVertex.x = mInversionVertex.x;
-		copyInversion.y = mVertex.y;
-		DrawLine3D(copyVertex, copyInversion, mColor);
-		copyVertex = mVertex;
-		copyInversion = mInversionVertex;
-
-		// 三本目
-		copyVertex.x = mInversionVertex.x;
-		copyVertex.y = mInversionVertex.y;
-		DrawLine3D(copyVertex, copyInversion, mColor);
-		copyVertex = mVertex;
-
-		// 四本目
-		copyInversion.x = mVertex.x;
-		copyVertex.y = mInversionVertex.y;
-		DrawLine3D(copyVertex, copyInversion, mColor);
-		copyVertex = mVertex;
-		copyInversion = mInversionVertex;
+			vertex[0] = VGet(mInversionVertex.x, mVertex.y, mVertex.z);
+			DrawTriangle3D(vertex[0], vertex[1], vertex[2], mColor, TRUE);
+		}
 	}
 }
