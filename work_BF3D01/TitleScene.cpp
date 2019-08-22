0d2543a8042584922da610c5b228f5f71749008e
DESKTOP-03G7PES\felid <felid.codas@gmail.com>
2019-08-22 12:55:34 +0900

    タイトルシーンを定義。描画関数を記述。

---
+++ work_BF3D01/TitleScene.cpp
@@ -0,0 +1,34 @@
+﻿#include "TitleScene.h"
+
+
+
+TitleScene::TitleScene():
+	SceneBase(10.0f, 3700.0f)
+{
+}
+
+
+TitleScene::~TitleScene()
+{
+}
+
+void TitleScene::Update(float deltaTime)
+{
+
+}
+
+void TitleScene::Draw()
+{
+	int x, y;
+	int w, h;
+	int lineCount;
+	int fontHandle = GameSystem::GetInstance().GetFontHandleForGoal();
+
+	const char * title = "いい感じのタイトル";
+	GetDrawStringSizeToHandle(&w, &h, &lineCount, title, strlen(title), fontHandle);
+
+	x = (GameSystem::GetInstance().GetScreenWidth() - w) / 2;
+	y = (GameSystem::GetInstance().GetScreenHeight() - h) / 2;
+
+	DrawStringToHandle(x, y, title, GetColor(255, 255, 255), fontHandle, GetColor(128, 0, 128));
+}
---
+++ work_BF3D01/TitleScene.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include "SceneBase.h"
+#include "GameSystem.h"
+
+class TitleScene : public SceneBase
+{
+public:
+	TitleScene();
+	~TitleScene();
+
+	void Update(float deltaTime)override;
+	void Draw() override;
+};
